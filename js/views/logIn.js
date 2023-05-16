2a6b17cce61bec6a4b93fb80a0622190afa31b97
ThyBui07 <buithinguyenthy@gmail.com>
2023-05-16 14:58:48 +0300

    fix the validation

--- js/views/logIn.js
+++ js/views/logIn.js
@@ -37,10 +37,10 @@ class logIn extends HTMLElement {
 
     render() {
         this.innerHTML = `
-        <form class="form-signin">
+        <form novalidate="novalidate" class="form-signin">
       <img class="mb-4" src="./favicon_io/favicon-32x32.png" alt="" width="72" height="72">
       <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-      <label for="inputEmail" class="sr-only">Email address</label>
+      <label for="inputEmail" class="sr-only">Email address or username</label>
       <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
       <label for="inputPassword" class="sr-only">Password</label>
       <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
